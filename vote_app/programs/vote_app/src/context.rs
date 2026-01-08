0297182947a80e5c5e605ec88b3870d1659eac76
Sumanthvu <sumanthveslavath07@gmail.com>
2026-01-08 20:15:37 +0530

    intialise all the accounts in conetxt

--- vote_app/programs/vote_app/src/context.rs
+++ vote_app/programs/vote_app/src/context.rs
@@ -1,5 +1,7 @@
 use anchor_lang::prelude::*;
 use crate::state::*;
+use anchor_spl::token::{Mint,Token,TokenAccount};
+
 #[derive(Accounts)]
 
 pub struct InitializeTreasury<'info>{
@@ -13,7 +15,24 @@ pub struct InitializeTreasury<'info>{
     seeds=[b"treasury_config"],
     bump
    )]
-   pub treasury_config_account:Account<'info,TreasuryConfig> 
+   pub treasury_config_account:Account<'info,TreasuryConfig> ,
+
+   #[account(
+    init,
+    payer=authority,
+    mint::authority=mint_authority,
+    mint::decimals=6,
+    seeds=[b"x_mint"],
+    bump
+)]
+   pub x_mint:Account<'info,Mint>,
+   pub treasury_token_account:Account<'info,TokenAccount>,
+
+   #[account(mut,seeds=[b"sol_vault"],bump)]
+   pub sol_vault:AccountInfo<'info>,
+
+   #[account(mut,seeds=[b"mint_authority"],bump)]
+   pub mint_authority:AccountInfo<'info>,
 
    pub system_program:Program<'info,System>
 }
